72df56d6f749e2c8bfb045767d6ffec8e46f76ae
alvarobello15 <164506215+alvarobello15@users.noreply.github.com>
2025-10-13 17:15:03 +0200

    Training_Yolov8.py

--- Training_Yolov8.py
+++ Training_Yolov8.py
@@ -7,9 +7,9 @@ model.train(
     epochs=15,
     patience=15,
     imgsz=640,
-    device=0,        # fuerza GPU
-    workers=2,       # 2 suele ir bien en Colab
-    batch=16, # ajusta según VRAM (8–32)
+    device=0,        
+    workers=2,       
+    batch=16, 
     project=SAVE_ROOT,
     name="plates_v8n",
     exist_ok=True
